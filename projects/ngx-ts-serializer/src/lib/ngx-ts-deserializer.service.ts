d8fd12e686f40b984118b12a6e093cae5436d147
DESKTOP-JBO1GAQ\Thomas Nisole <thomasnisole@gmail.com>
2018-10-10 09:10:47 +0200

    Add deserializer

---
+++ projects/ngx-ts-serializer/src/lib/ngx-ts-deserializer.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {Deserializer} from 'ts-serializer-core';
+
+@Injectable()
+export class NgxTsDeserializerService {
+
+  public constructor(private deserializer: Deserializer) { }
+
+  public deserialize(type: {new (): any}, object: any | any[]): any {
+    return this.deserializer.deserialize(type, object);
+  }
+}
--- projects/ngx-ts-serializer/src/lib/ngx-ts-serializer.module.ts
+++ projects/ngx-ts-serializer/src/lib/ngx-ts-serializer.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from '@angular/core';
 import {NgxTsSerializerService} from './ngx-ts-serializer.service';
-import {ConverterStrategy, Serializer, SerializerConfiguration} from 'ts-serializer-core';
+import {ConverterStrategy, Deserializer, Serializer, SerializerConfiguration} from 'ts-serializer-core';
 import {SERIALIZER_CONFIGURATION, SERIALIZER_CONVERTER_STRATEGY} from './ngx-ts-serializer.di';
+import {NgxTsDeserializerService} from './ngx-ts-deserializer.service';
 
 export function serializerFactory(configuration: SerializerConfiguration, converterStrategy: ConverterStrategy) {
   return new NgxTsSerializerService(new Serializer(configuration, [converterStrategy]));
+}
+
+export function deserializerFactory(converterStrategy: ConverterStrategy) {
+  return new NgxTsDeserializerService(new Deserializer([converterStrategy]));
 }
 
 @NgModule({
@@ -13,6 +18,11 @@ export function serializerFactory(configuration: SerializerConfiguration, conver
       provide: NgxTsSerializerService,
       useFactory: serializerFactory,
       deps: [SERIALIZER_CONFIGURATION, SERIALIZER_CONVERTER_STRATEGY]
+    },
+    {
+      provide: NgxTsSerializerService,
+      useFactory: serializerFactory,
+      deps: [SERIALIZER_CONVERTER_STRATEGY]
     }
   ]
 })
